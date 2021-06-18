62552a2ee495f278a62c11a99e67c9cc1c4a0b3b
aaron isaac <aaron@aisaac.co>
2021-06-17 22:57:11 -0600

    Fixed websockets to what it was

--- src/main/java/com/ajisaac/scrapebatch/websocket/MessageService.java
+++ src/main/java/com/ajisaac/scrapebatch/websocket/MessageService.java
@@ -1,20 +1,52 @@
 package com.ajisaac.scrapebatch.websocket;
 
-import javax.inject.Singleton;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//
-@Singleton
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+@ApplicationScoped
+@ServerEndpoint("/topic/messages")
 public class MessageService {
-//
-//  private final SimpMessagingTemplate template;
-//
-//  public MessageService(@Autowired SimpMessagingTemplate template) {
-//    this.template = template;
-//  }
-//
+
+
+  private Session session;
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @OnOpen
+  public void onOpen(Session session) {
+    this.session = session;
+  }
+
+  @OnClose
+  public void onClose() {
+    this.session = null;
+  }
+
+  @OnError
+  public void onError(Throwable throwable) {
+    broadcast("{error: " + throwable + "}");
+  }
+
+  @OnMessage
+  public void onMessage(String message) {
+    System.out.println(message);
+  }
+
+  private void broadcast(String message) {
+    if (this.session != null) {
+      session.getAsyncRemote().sendObject(message);
+    }
+  }
+
   public void send(Message message) {
-//    template.convertAndSend("/topic/messages", message);
+    try {
+      String msg = mapper.writeValueAsString(message);
+      broadcast(msg);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
   }
 }
